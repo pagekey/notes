7218b96ab3073cd6f0020746db7328adf56edd16
Stephen Grice <stephengrice@live.com>
2024-08-30 04:18:33 -0400

    Call start method on xylo

---
+++ backend/src/main.rs
@@ -0,0 +1,13 @@
+// placeholder
+
+// fn hello_route(name: &str) -> String {
+//     format!("Hello, {}", name)
+// }
+
+// pub const ROUTES: &[fn()] = &[hello_route];
+
+use pagekey_xylo::start;
+
+fn main() {
+    start();
+}
--- backend/src/route.rs
+++
@@ -1,7 +0,0 @@
-// placeholder
-
-fn hello_route(name: &str) -> String {
-    format!("Hello, {}", name)
-}
-
-pub const ROUTES: &[fn()] = &[hello_route];
