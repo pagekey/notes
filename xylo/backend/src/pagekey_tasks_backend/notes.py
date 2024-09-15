c4e4ae12188b4ebcea4fa597e073b9a0b5ff4eb1
Stephen Grice <stephengrice@live.com>
2024-09-15 15:11:08 -0400

    Insert notes into sqlite db

--- xylo/backend/src/pagekey_tasks_backend/notes.py
+++ xylo/backend/src/pagekey_tasks_backend/notes.py
@@ -1,9 +1,35 @@
+import sqlite3
+
+
+def create_table_if_not_exists():
+    conn = sqlite3.connect("tasks.db")
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS notes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        body TEXT NOT NULL,
+        created DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated DATETIME DEFAUlt CURRENT_TIMESTAMP
+    )
+    ''')
+    conn.commit()
+    conn.close()
+
+def create_note(body: str):
+    create_table_if_not_exists()
+    conn = sqlite3.connect("tasks.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO notes (body) VALUES (?)", (body,))
+    conn.commit()
+    conn.close()
+
 def save(body: dict):
-    print("hey just got hte body:",body)
     if "note" in body:
         note = body["note"]
-        with open("notes.txt", "a") as f:
-            f.write(f"the note: {note}\n")
-    return {
-        "message": "saved",
-    }
+        create_note(note)
+        return {
+            "message": "saved to db",
+        }
+    else:
+        return {
+            "message": "no note provided"
+        }
